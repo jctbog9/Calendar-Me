52614762caba5c51c87722de8a0bd61ec9e74056
d.mui2383 <d.mui2383@gmail.com>
2018-11-21 00:32:59 -0500

    added description, logo, event-start and event-end to index page; basic css added

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,7 +4,7 @@ class EventsController < ApplicationController
     @events_list = []
     events_data = HTTParty.get("https://www.eventbriteapi.com/v3/events/search/?location.address=boston&subcategories=2003%2C2001%2C2002&search_type=networking&token=JLHHNB4KRCZWR7WZPKAY")
     events_data['events'].each do |event|
-      @events_list << event['name']['text']
+      @events_list << event
     end
   end
 end
