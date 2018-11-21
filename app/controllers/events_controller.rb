56106119b450656fc3b2366941b8ba32a7e411fe
d.mui2383 <d.mui2383@gmail.com>
2018-11-21 00:43:28 -0500

    index page with name, description, logo, start and end times; basic css and links

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,7 +2,7 @@ class EventsController < ApplicationController
   def index
     @events = Event.all
     @events_list = []
-    events_data = HTTParty.get("https://www.eventbriteapi.com/v3/events/search/?location.address=boston&subcategories=2003%2C2001%2C2002&search_type=networking&token=JLHHNB4KRCZWR7WZPKAY")
+    events_data = HTTParty.get("https://www.eventbriteapi.com/v3/events/search/?location.address=boston&subcategories=2003%2C2001%2C2002&search_type=networking&token=W6RPJZTBWTTQMJUDOSCB")
     events_data['events'].each do |event|
       @events_list << event
     end
