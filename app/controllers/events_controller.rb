7d26524fcd8d36b5a633e1868f7c9f9bf2a5d30a
d.mui2383 <d.mui2383@gmail.com>
2018-11-26 20:48:23 -0500

    fixed index page when there's no logo, created onClick function

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -7,4 +7,26 @@ class EventsController < ApplicationController
       @events_list << event
     end
   end
+
+  def show
+    @event = Event.find(params['id'])
+  end
+
+  def create
+    @event = Event.new(event_params)
+    if @event.save
+      flash[:notice] = "Event Added!"
+      render events_path
+    else
+      flash[:notice] = @event.errors.full_messages.join(' | ')
+      render events_path
+    end
+  end
+
+
+  private
+
+  def event_params
+    params.require(:event).permit(:name, :address, :city, :state, :zip, :date, :start_time, :end_time, :description, :ticket_price)
+  end
 end
