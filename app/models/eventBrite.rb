9c693b4b83cf3c3336952c425c8deff296a5c9e9
John Trocchi <jctbog9@aol.com>
2018-12-18 00:22:50 -0500

    bug fix

--- app/models/eventBrite.rb
+++ app/models/eventBrite.rb
@@ -1,5 +1,4 @@
-require 'HTTParty'
-require 'pry'
+require 'httparty'
 
 @event_data = HTTParty.get("https://www.eventbriteapi.com/v3/events/search/?location.address=boston&subcategories=2003%2C2001%2C2002&search_type=networking&token=W6RPJZTBWTTQMJUDOSCB")
 @event_data['events'].each do |event|
