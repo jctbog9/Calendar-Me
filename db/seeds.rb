805741a2b951d5fbbc0f8dc9d999ac328e8655f9
Brendon Shea <brendonshea@Brendons-MacBook-Air.local>
2018-12-28 12:35:14 -0500

    Demo

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,4 +7,4 @@ Event.create!(name: 'THE Event', location: '333 Wyman Street Waltham MA', date: 
 Event.create!(name: 'Second Event', location: '333 Wyman Street Waltham MA', date: '11/16/18', time: '2:00PM - 4:00PM', description: 'This is another description', url: 'http://www.facebook.com')
 
 Signup.create!(user_id: 1, event_id: 1)
-User.create!(email: 'admin@gmail.com', password: 'admin1', first_name: 'John', last_name: 'Smith', business_phone: '6176176177', team_id: 1, role: "admin")
+User.create!(email: 'admin@gmail.com', password: 'admin1', first_name: 'Bill', last_name: 'Butler', business_phone: '6176176177', team_id: 1, role: "admin")
