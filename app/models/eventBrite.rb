b0ea25f2d1455c771224d86d9d91a3b0ce58b89f
John Trocchi <jctbog9@aol.com>
2018-12-28 10:10:39 -0500

    made eventbrite scraper a ruby class

--- app/models/eventBrite.rb
+++ app/models/eventBrite.rb
@@ -1,16 +1,21 @@
 require 'httparty'
 
-@event_data = HTTParty.get("https://www.eventbriteapi.com/v3/events/search/?location.address=boston&subcategories=2003%2C2001%2C2002&search_type=networking&token=W6RPJZTBWTTQMJUDOSCB")
-@event_data['events'].each do |event|
-  time = Time.parse(event['start']['utc']).getutc.strftime("%I:%M")
-  end_time = Time.parse(event['end']['utc']).getutc.strftime("%I:%M")
-  eventData = {
-    name: event['name']['text'],
-    description: event['description']['text'],
-    time: time,
-    date: event['start']['utc'],
-    url: event['url'],
-    event_id: event['id']
-  }
-  Event.create!(eventData)
+class Eventbrite
+
+  def self.scrape
+    @event_data = HTTParty.get("https://www.eventbriteapi.com/v3/events/search/?location.address=boston&subcategories=2003%2C2001%2C2002&search_type=networking&token=W6RPJZTBWTTQMJUDOSCB")
+    @event_data['events'].each do |event|
+      time = Time.parse(event['start']['utc']).getutc.strftime("%I:%M")
+      end_time = Time.parse(event['end']['utc']).getutc.strftime("%I:%M")
+      eventData = {
+        name: event['name']['text'],
+        description: event['description']['text'],
+        time: time,
+        date: event['start']['utc'],
+        url: event['url'],
+        event_id: event['id']
+      }
+      Event.create!(eventData)
+    end
+  end
 end
